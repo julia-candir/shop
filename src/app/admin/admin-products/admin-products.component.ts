895048294ae417eac38db030eea97aa1db31a6b4
Julia Candir <julia.candir@gmail.com>
2020-11-10 17:41:45 +0300

    datatable implementation

--- src/app/admin/admin-products/admin-products.component.ts
+++ src/app/admin/admin-products/admin-products.component.ts
@@ -12,6 +12,7 @@ export class AdminProductsComponent implements OnInit, OnDestroy {
   products: Product[];
   filteredProducts: Product[];
   subscription: Subscription;
+  dtOptions: DataTables.Settings = {};
 
   constructor(private productService: ProductService) {
     this.subscription = this.productService.getAllProducts().subscribe((products: Product[]) => this.filteredProducts = this.products = products);
@@ -21,7 +22,15 @@ export class AdminProductsComponent implements OnInit, OnDestroy {
     this.filteredProducts = query ? this.products.filter(product => product.title.toLocaleLowerCase().includes(query.toLocaleLowerCase())) : this.products;
   }
 
-  ngOnInit() { }
+  ngOnInit() {
+    this.dtOptions = {
+      pagingType: 'full_numbers',
+      searching: false,
+      columnDefs: [
+        { orderable: false, targets: 2 }
+      ]
+    };
+  }
 
   ngOnDestroy() {
     this.subscription.unsubscribe();
