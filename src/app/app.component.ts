47ebca03c5359b2ad964932847d0ffcf1ef1067d
Julia Candir <julia.candir@gmail.com>
2020-11-16 15:15:58 +0300

    redirection on refresh bug fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,7 +17,10 @@ export class AppComponent implements OnDestroy {
       if (user) {
         userService.save(user);
         let returnUrl = localStorage.getItem('returnUrl');
-        router.navigateByUrl(returnUrl);
+        if (returnUrl) {
+          localStorage.removeItem('returnUrl');
+          router.navigateByUrl(returnUrl);
+        }
       }
     });
     this.subscriptions = [...this.subscriptions, sub];
