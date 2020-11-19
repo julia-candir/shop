874f780850ef16d093488288ca9a0ef3583d75d0
Julia Candir <julia.candir@gmail.com>
2020-11-19 18:06:37 +0300

    product card and filter comp implemented

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,11 +1,8 @@
-import { Router, ActivatedRoute } from '@angular/router';
-import { CategoryService } from './../shared/services/category.service';
+import { ActivatedRoute } from '@angular/router';
 import { Component } from '@angular/core';
 import { ProductService } from '../shared/services/product.service';
-import { Category } from '../shared/models/category';
-import { map } from 'rxjs/operators';
+import { switchMap } from 'rxjs/operators';
 import { Product } from '../shared/models/product';
-import { THROW_IF_NOT_FOUND } from '@angular/core/src/di/injector';
 
 @Component({
   selector: 'app-products',
@@ -15,16 +12,15 @@ import { THROW_IF_NOT_FOUND } from '@angular/core/src/di/injector';
 export class ProductsComponent {
   products: Product[] = [];
   filteredProducts: Product[] = [];
-  categories$;
   category: string;
 
-  constructor(productService: ProductService, categoryServise: CategoryService, route: ActivatedRoute) { 
-    productService.getAllProducts().subscribe((products: Product[]) => this.products = products);
-    this.categories$ = categoryServise.getAllCategories().pipe(map(data => data.map(item => item.payload.doc.data() as Category)));
-
-    route.queryParamMap.subscribe(params => {
-      this.category = params.get('category');
-      this.filteredProducts = (this.category) ? this.products.filter(p => p.category.shortcode === this.category) : this.products;
-    })
+  constructor(productService: ProductService, route: ActivatedRoute) { 
+    productService.getAllProducts().pipe(switchMap((products: Product[]) => {
+      this.products = products;
+      return route.queryParamMap;
+    })).subscribe(params => {
+        this.category = params.get('category');
+        this.filteredProducts = (this.category) ? this.products.filter(p => p.category.shortcode === this.category) : this.products;
+      });
   }
 }
